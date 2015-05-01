f19bbe1b996ac8937de4440cea88985dada6cacc
ramihelal <rami.talat@gmail.com>
2015-05-01 07:06:27 +0200

    Update install_odoo.sh

--- Buraq/VM/install_odoo.sh
+++ Buraq/VM/install_odoo.sh
@@ -15,17 +15,19 @@
 # Get the odoo_install script
 wget https://raw.githubusercontent.com/ramitalat/Ascend_Solutions/master/Buraq/VM/odoo_install_asch.sh
 
+# Get github.com/mohamedhagag/dvit-odoo8 modules
+# Thanks to Mohamed Hagag for his great work
+cd $OE_HOME/custom/addons
+wget https://raw.githubusercontent.com/ramitalat/Ascend_Solutions/master/Buraq/VM/extra_apps.sh
+sudo sh extra_apps.sh
+
 chmod +x odoo_install_asch.sh 
 sudo ./odoo_install_asch.sh 2>&1 | tee odoo_installation.log
 
-# Get the backup script
-
+# Prepare the backup ...
 # Location to place backups.
 backup_dir="/var/backups/postgres_db"
 
-# run the extention sript to odoo_install.sh
-wget https://raw.githubusercontent.com/ramitalat/Ascend_Solutions/master/Buraq/VM/script_ext.sh
-sudo sh script_ext.sh
 sudo mkdir $backup_dir
 sudo chown postgres $backup_dir
 
